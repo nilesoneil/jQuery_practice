371f2e65375d8c49b4e260d8aa1c829c80f7e908
Niles O'Neil <nilesoneil@gmail.com>
2022-03-29 19:56:41 -0700

    learned $ jQuery shortcut
    co-authored-by: Louie Knolle <knollelw@gmail.com>
    co-authored-by: Viktoria Zubarieva <viktoria.dubinina@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-jQuery("h1").click(function() {
-  alert("This is a heading.");
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a heading.");
+    alert("I told you, THIS IS A HEADING!");
 });
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
 });
 
-jQuery("img").click(function() {
-  alert("This is an image.");
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
 });
